c69339e830997df70120af9a9db088770c7de3e1
Soraya Povedano <72301609+sorayapg@users.noreply.github.com>
2024-09-19 18:32:45 +0200

    sass, JavaScrip y Jquery

---
+++ practicas/sass-app/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    //..other config
+    css: {
+      preprocessorOptions: {
+        scss: {
+          silenceDeprecations: ["legacy-js-api"],
+        },
+      },
+    },
+  })
